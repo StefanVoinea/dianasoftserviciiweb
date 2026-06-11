371034b86f0958cb81484edc33484fd0a62851d6
StefanVoinea <stefan.voinea@gmail.com>
2026-06-11 11:50:55 +0300

    Login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -19,42 +19,29 @@ class AuthController extends Controller
     }
     public function login(Request $request)
     {
-       
-    	//$http = new \GuzzleHttp\Client;
-       
-        
         try {
-            // $response = $http->post(config('services.passport.login_endpoint'), [
-            //     'form_params' => [
-            //         'grant_type' => 'password',
-            //         'client_id' => config('services.passport.client_id'),
-            //         'client_secret' => config('services.passport.client_secret'),
-            //         'username' => $request->username,
-            //         'password' => $request->password,
-            //         ]
-            //         ]);
-            $request = Request::create('/oauth/token', 'POST', [
-                'grant_type'    => 'password',
-                'client_id'     => config('passport.password_client.id'),
-                'client_secret' => config('passport.password_client.secret'),
-                'username'      => $request->email,
-                'password'      => $request->password,
-                'scope'         => '',
-            ]);
-            $response = app()->handle($request);        
+            $http = new \GuzzleHttp\Client();
             
-    		return $response->getBody();
+            $response = $http->post(config('services.passport.login_endpoint'), [
+                'form_params' => [
+                    'grant_type'    => 'password',
+                    'client_id'     => config('services.passport.client_id'),
+                    'client_secret' => config('services.passport.client_secret'),
+                    'username'      => $request->email ?? $request->username,
+                    'password'      => $request->password,
+                    'scope'         => '',
+                ]
+            ]);
+            
+            return response($response->getBody(), $response->getStatusCode());
     	
-    	} catch (\Exception $e) // catch (\GuzzleHttp\Exception\BadResponseException $e) 
-        {
-    		if($e->getCode()==400) {
-    			return response()->json('Invalid Request, Please enter a username or a password.',$e->getCode());
-    		} else if ($e->getCode()==401) {
-    			return response()->json('Your credentials are incorrect. Please try again',$e->getCode());
-    		}
-           
-    		return response()->json('Something went wrong on the server.', $e->getCode() );
-    	};
+    	} catch (\GuzzleHttp\Exception\BadResponseException $e) {
+    		$status = $e->getResponse()->getStatusCode();
+    		$body = (string) $e->getResponse()->getBody();
+    		return response($body, $status);
+        } catch (\Exception $e) {
+    		return response()->json('Authentication server error.', 500);
+    	}
     
     // }  catch (\GuzzleHttp\Exception\BadResponseException $e) 
     //     {
